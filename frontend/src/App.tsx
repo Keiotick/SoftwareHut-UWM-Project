03fb7e3c27e8dd94c9c8a4a2964c5ab95b50f2aa
Keiotick <szymon.wylota@gmail.com>
2021-04-29 13:28:48 +0200

    added movie and home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,52 +1,25 @@
 import React from 'react';
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
+
+import Movie from './views/movie/Movie';
+import Home from './views/home/Home';
+import Page404 from './views/page404/Page404';
 import './App.css';
 
-//import CustomHeader, { addNumber } from './components/CustomHeader';
-import Counter from './components/Counter';
-import CustomHeader from './components/CustomHeader';
-
 const App = () => {
-  //const addedNumber = addNumber(1, 2);
-   const [changedNumber, setChangedNumber ] = React.useState(0);
 
-  const handleNumberChange = (newNumber: number) => {
-    setChangedNumber(newNumber);
-    //console.log(newNumber)
-   // return newNumber;
-  }
-  
-  const greaterThen10 = (liczba: number) =>
-  {
-    if (liczba > 10 && liczba < 16){
-      return (<div>Liczba jest większa od 10</div>)
-    }
-  }
-
-  const greaterThen15 = (liczba: number) =>
-  {
-    if (liczba > 15){
-      return (<div>Liczba przekroczona</div>)
-    }
-  }
-
-
-  return (
+  return(
     <div className="App">
-      <CustomHeader>  
-              
-        {/* <Counter onNumberChange={(newNumber)=>{ console.log(newNumber) }}/> */}
-        {/* <ResetValue onNumberChange={handleNumberChange}/> */}
-        <Counter onNumberChange={handleNumberChange}/>
-        {
-          changedNumber < -10 && (<div> Liczba jest mniejsza od -10</div>)
-        }
-        {greaterThen10(changedNumber)}
-        {greaterThen15(changedNumber)}
-      </CustomHeader>
-      
+      <BrowserRouter>
+        <Switch>
+          <Route path="/movie/:id" component={Movie} />
+          <Route path='/' component={Home} exact />
+          <Route path="*" component={Page404} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
-
 }
+
 
 export default App;
