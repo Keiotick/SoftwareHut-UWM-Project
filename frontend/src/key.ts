b9338a755be961146c43207a530935a1706ce52b
Keiotick <szymon.wylota@gmail.com>
2021-05-14 20:31:18 +0200

    added omdb api

---
+++ frontend/src/key.ts
@@ -0,0 +1,1 @@
+export const apikey = 'b387a1a1';
--- frontend/src/views/home/Home.tsx
+++ frontend/src/views/home/Home.tsx
@@ -1,13 +1,50 @@
 import React from 'react';
 import NavPanel from '../../components/NavPanel';
+import movieService from '../../services/movies.service';
+//import { omdApiKey } from '../../key';
+//import http from '../../utulis/http';
 
 const Home = () => {
-    return (
-        <div>
+    const [search, setSearch] = React.useState('harry potter');
+    const [movie, setMovie] = React.useState({});
+
+    /*
+    React.useEffect(() => {
+        const makeRequest = asynq () => {
+
+        try{
+            const responce = await fetch(`http://www.omdbapi.com/?apikey=${omdApiKey}&s=harry+potter&plot=full`).then(resp => resp.json());
+            console.log(responce);
+        }
+        catch (error){
+            console.log(error);
+        }
+
+        };
+        makeRequest();
+    },[search]);
+    */
+
+
+
+    React.useEffect(() => {
+        const makeRequest = async () => {
+            try {
+
+            const responce = movieService.searchByName(search,1);
+            
+            setMovie(responce);
+            }catch(error){
+                console.log(error);
+            }
+        };
+        makeRequest();
+    }, [search]);
+   return (
+        <div> 
             <NavPanel />
-            <div> This is home page</div>
+            <div>This is home page</div>
         </div>
     );
 }
-
 export default Home;
